e95b07340b7d54bbf9a821907c3e36984c607f07
Nikita Alekseev <n.alekseev2104@gmail.com>
2017-09-17 23:14:02 +0300

    Added token.h file, representing lexical token. Types are to be added as needed

---
+++ src/token.h
@@ -0,0 +1,23 @@
+#include <string>
+
+namespace lexer 
+{
+
+enum class TokenType 
+{
+    If, Else, For, While
+    //To be continued
+} 
+
+class Token
+{
+public:
+    TokenType type;
+    std::string value;
+
+    Token(TokenType type, std::string value): type{type}, value{value} {};
+
+    Token(TokenType type) type{type}, value{""}
+}
+
+}
