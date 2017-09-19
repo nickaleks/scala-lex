494954a3684810df9196eb57ecaf541a02d8be3e
Nikita Alekseev <n.alekseev2104@gmail.com>
2017-09-19 19:53:23 +0300

    Added whitespace and if keywords parsing

--- src/lexer.cpp
+++ src/lexer.cpp
@@ -6,7 +6,14 @@ void lexer::Lexer::scan() {
     auto it = source.begin();   
     while(it != source.end()) {
         auto word =  getWord(it);
-        
+        if (word.len == 1 && *word.it == ' ') {
+            buf.emplace_back(TokenType::Whitespace);
+        } else if (
+            word.len == 2 && 
+            *word.it == 'i' &&
+            *(word.it + 1) == 'f') {
+            buf.emplace_back(TokenType::If);
+        }
         it += word.len;
     }
 }
