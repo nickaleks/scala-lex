e51936d3a569dc03bb109eac79ac621daa9a2e70
Nikita Alekseev <n.alekseev2104@gmail.com>
2017-09-28 14:02:26 +0300

    Fixed wrong assignment

--- src/string_slice.h
+++ src/string_slice.h
@@ -1,6 +1,6 @@
 #ifdef __cpp_lib_string_view
 #include <string_view>
-using StringSlice = StringSlice;
+using StringSlice = std::string_view;
 #else
 #include <experimental/string_view>
 using StringSlice = std::experimental::string_view;
